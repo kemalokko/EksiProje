be5b97f7e5ac30092149f3f225144c47a48ef05c
taha <tahakeskin@windowslive.com>
2018-07-11 14:39:08 +0300

    Revert "asda"
    This reverts commit b63b906a839ef08055d2fcf85532860b80c4df52.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,10 +13,6 @@ namespace ConsoleApplication1
             Console.WriteLine("asda");
             Console.WriteLine("12");
             Console.WriteLine("ew");
-            Console.WriteLine("ew");
-
-            Console.WriteLine("ew");
-
         }
     }
 }
